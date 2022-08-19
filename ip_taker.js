073519ab00fac8e275f78a21647340a5278fc5a1
Creater2 <105660444+Creater2@users.noreply.github.com>
2022-08-20 00:46:59 +0300

    Add files via upload

---
+++ ip_taker.js
@@ -0,0 +1,18 @@
+$.getJSON("https://api.ipify.org/?format=json", function(e) {
+    console.log(e.ip);
+    var d = new Date();
+    var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    console.log(time);
+
+    let input_for_ip = document.querySelector('.input_for_ip')
+    input_for_ip.value = ('ip адресс: ' + e.ip + '  Время перехода: ' + time);
+    let submit_activation = document.querySelector('.ip_form').submit()
+    
+
+
+    console.log('ip адресс:' + e.ip);
+
+});
+
+
+ 
